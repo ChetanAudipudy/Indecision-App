abf7f2a28d3f87911920ff8ac4a0cdabbfc04c37
Chetan Audipudy <31904464+ChetanAudipudy@users.noreply.github.com>
2018-03-22 15:28:21 -0500

    Delete es6-arrow-function.js

--- src/playgrounds/es6-arrow-function.js
+++
@@ -1,8 +0,0 @@
-const multiplier = {
-    numbers : [1 ,6],
-    multiplyBy : 2,
-    multiply(){
-        return this.numbers.map((num) => this.multiplyBy * num);
-    }
-}
-console.log(multiplier.multiply())
